a906b45002daf8904658c3736001e01de2411b12
DionEmary <emarydion15@gmail.com>
2025-01-19 17:49:17 -0700

    Added Header, Main and Footer to the document with styling roughly matching Twitter

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+
+      </View>
+      <ScrollView>
+        <View style={styles.main}>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus totam optio minus suscipit illum reiciendis et? Distinctio saepe quae libero doloremque alias! Repudiandae deserunt quidem nesciunt corrupti saepe vel possimus!
+          </Text>
+        </View>
+      </ScrollView> 
+      <View style={styles.footer}>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +24,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(0, 0, 0, 1)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    position: 'absolute',
+    top: 0,
+    zIndex: 100,
+    backgroundColor: 'rgba(7, 7, 7, 0.90)',
+    width: Dimensions.get('window').width,
+    height: 146.5,
+    borderBottomWidth: 1,
+    borderBottomColor: 'rgb(49, 52, 55)', 
+  },
+  main: {
+    height: 800,
+    marginTop: 146.5,
+    width: Dimensions.get('window').width,
+    backgroundColor: 'rgba(0, 0, 0, 1)',
+  },
+  footer: {
+    width: Dimensions.get('window').width,
+    height: 90,
+    backgroundColor: 'rgba(7, 7, 7, 0.90)',
+    borderTopWidth: 1,
+    borderTopColor: 'rgb(49, 52, 55)', 
+  },
+  text: {
+    color: '#ffffff',
+  }
 });
